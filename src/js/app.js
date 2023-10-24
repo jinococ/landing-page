b423d12c3421cc0b1300c44ff3caf4d4641f2df9
Nikolay Bezmelitsyn <nikolay.bezmelicyn@sigma.software>
2023-10-24 08:15:57 +0300

    Initial commit. Setup gulp config

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+/*
+For example:
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
---
+++ src/js/modules/functions.js
@@ -0,0 +1,15 @@
+/* Check if browser supports webp */
+export function isWebp() {
+	function testWebP(callback) {
+		let webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
+	// Add class _webp or _no-webp for HTML
+	testWebP(function (support) {
+		let className = support === true ? 'webp' : 'no-webp';
+		document.documentElement.classList.add(className);
+	});
+}
